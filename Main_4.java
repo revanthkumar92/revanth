70ac95d0683bf034a44ea3da71768ab9ffbc1f20
revanthkumar92 <bandelarevanth013@gmail.com>
2025-08-04 14:59:53 +0530

    Add files via upload

---
+++ Main_4.java
@@ -0,0 +1,12 @@
+class Main_4{
+public static void main(String[] args){
+int a=12,b=12;
+int result1,result2;
+System.out.println("a value is:"+a);
+result1=++a;
+System.out.println("after increment:"+result1);
+System.out.println("b value is:"+b);
+result2=--b;
+System.out.println("after decrement:"+result2);
+}
+}
