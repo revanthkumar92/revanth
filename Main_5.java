cd388f04ec46caa4b848de665de879c5ba61f9b8
revanthkumar92 <bandelarevanth013@gmail.com>
2025-08-04 15:00:22 +0530

    Add files via upload

---
+++ Main_5.java
@@ -0,0 +1,12 @@
+class Main_5{
+public static void main(String[] args){
+int a=7,b=11;
+System.out.println("a value is:"+a+"b value is:"+b);
+System.out.println(a==b);
+System.out.println(a!=b);
+System.out.println(a>b);
+System.out.println(a<b);
+System.out.println(a>=b);
+System.out.println(a<=b);
+}
+}
