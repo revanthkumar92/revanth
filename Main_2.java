1dddb7d257d6ef79dd5e30bb8decf8459d8505d7
revanthkumar92 <bandelarevanth013@gmail.com>
2025-08-04 14:58:51 +0530

    Add files via upload

---
+++ Main_2.java
@@ -0,0 +1,8 @@
+class Main_2{
+public static void main(String[] args){
+String str="programiz";
+boolean result;
+result=str instanceof String;
+System.out.println("is str an object of string?"+result);
+}
+}
