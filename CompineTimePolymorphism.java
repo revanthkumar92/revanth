42e40a246b421d0275a6b299130fdc4ce9ca5ac4
Revanth kumar <bandelarevanth013@gmail.com>
2025-09-22 15:51:15 +0530

    Add files via upload

--- CompineTimePolymorphism.java
+++ CompineTimePolymorphism.java
@@ -1,34 +1,22 @@
-class Vehicle{
-	public void startEngine(){
-		System.out.println("the vehicle engine is starting");
-	}
+class Calculator{
+public int add(int a,int b){
+System.out.println("adding the two numbers:");
+return a+b;
 }
-
-class Car extends Vehicle{
-	@Override
-	public void startEngine(){
-		System.out.println("the car's engine is starting with a key ignition");
-	}
+public int add(int a,int b,int c){
+System.out.println("adding the three integers:");
+return a+b+c;
 }
-
-class Motorcycle extends Vehicle{
-	@Override
-	public void startEngine(){
-		System.out.println("the motor cycle is started with a kick start");
-	}
+public double add(double a,double b){
+System.out.println("adding the two doubles:");
+return a+b;
 }
-
-public class CompineTimePolymorphism {
-	public static void main(String[] args){
-		Vehicle myvehicle;
-		myvehicle = new Car();
-		myvehicle.startEngine();
-		myvehicle = new Motorcycle();
-		myvehicle.startEngine();
-		System.out.println("direct object calls:");
-		Car mycar = new Car();
-		Motorcycle mymc = new Motorcycle();
-		mycar.startEngine();
-		mymc.startEngine();
-	}
+}
+public class CompineTimePolymorphism{
+public static void main(String[] args){
+Calculator calc=new Calculator();
+System.out.println("Result 1:"+calc.add(10,20));
+System.out.println("Result 2:"+calc.add(10,20,30));
+System.out.println("Result 3:"+calc.add(10.5,20.5));
+}
 }
